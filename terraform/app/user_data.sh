068e353f554df022bdf31df97bf3d82aa9a893e7
shoukoo <thebealox@gmail.com>
2020-01-12 10:04:14 +1100

    Script to monitor logging

--- terraform/app/user_data.sh
+++ terraform/app/user_data.sh
@@ -1,6 +1,45 @@
 #!bin/bash
 set -exuo pipefail
+echo "- Starting user_data"
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+usermod -a -G docker ec2-user
 
-echo "=================== user data ==================="
-echo "===================  done ==================="
 
+# Install Git
+sudo yum install -y git
+git clone https://github.com/shoukoo/rea.git
+cd rea
+docker build -t rea:latest .
+docker run -p 9292:9292 -d rea:latest
+
+# Install nginx
+sudo yum install -y nginx
+sudo bash -c 'cat << "EOF" > /etc/nginx/conf.d/default.conf
+user nginx;
+
+events {
+  worker_connections  1024;
+}
+
+http {
+  upstream sinatra {
+    server localhost:9292;
+  }
+
+  access_log  /var/log/nginx/access.log;
+
+  server {
+    listen   80;
+    server_name  $hostname;
+
+    location / {
+      proxy_pass http://sinatra;
+    }
+  }
+}
+EOF'
+sudo sed -i 's/NGINX_CONF_FILE=.*/NGINX_CONF_FILE=\/etc\/nginx\/conf\.d\/default.conf/g' /etc/sysconfig/nginx
+sudo service nginx start
+touch /tmp/finished-user-data
--- terraform/app/wait_for_user_data.sh
+++ terraform/app/wait_for_user_data.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-set -exou pipefail
-
+# ssh uses exit status 255 to mean can't connect
+ex=255
+while [ $ex -eq 255 ]; do
+  sleep 10
+  ssh -i ../../rea -o StrictHostKeyChecking=no ec2-user@${1} -- \
+    'timeout 360 sed -u /finished-user-data/q <(tail -n 1000 -f /var/log/cloud-init-output.log)'
+  ex=$?
+done
+exit $ex
 
 echo $1
